29ecf2b7ea85bb85979495c5d6f1f08586f83e14
Rahul Kathuria <rahulkathuria52@gmail.com>
2022-07-12 00:58:54 +0530

    Added Reset button

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -6,6 +7,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late Stopwatch stopwatch;
+  void handleStartStop() {}
+
+  String returnFormattedText() {
+    return "";
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    stopwatch = Stopwatch();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,16 +29,31 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-             alignment: Alignment.center,
-              height: 250,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                  border: Border.all(color: Colors.blue, width: 10)),
-              child: const Text(
-                "00:00:000",
-                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            CupertinoButton(
+              onPressed: () {},
+              child: Container(
+                alignment: Alignment.center,
+                height: 250,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.blue, width: 10)),
+                child: const Text(
+                  "00:00:000",
+                  style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                ),
               ),
+            ),
+            CupertinoButton(
+              child: Text(
+                "Reset",
+                style:
+                    TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+              ),
+              onPressed: () {},
+              padding: EdgeInsets.all(0),
             )
           ],
         ),
