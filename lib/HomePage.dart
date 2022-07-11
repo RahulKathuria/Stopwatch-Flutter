c39958564a4baf3be0bd5041eea2e7876b55d6bd
Rahul Kathuria <rahulkathuria52@gmail.com>
2022-07-12 00:49:54 +0530

    Created UI for stopwatch

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
@@ -10,6 +8,26 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+             alignment: Alignment.center,
+              height: 250,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                  border: Border.all(color: Colors.blue, width: 10)),
+              child: const Text(
+                "00:00:000",
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              ),
+            )
+          ],
+        ),
+      )),
+    );
   }
 }
